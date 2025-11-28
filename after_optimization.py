a00d913ebe829cb8eb7b6f65b4aed779c479b755
naumoffIE <asfer364@gmail.com>
2025-11-28 15:35:46 +0400

    added first optimized function

--- after_optimization.py
+++ after_optimization.py
@@ -4,6 +4,21 @@ from collections import Counter, defaultdict
 import cProfile
 
 
+def process_data_efficient(data):
+    """Эффективная обработка данных"""
+    result = []
+
+    index_map = defaultdict(list)
+    for i, value in enumerate(data):
+        index_map[value].append(i)
+
+    for value, indices in index_map.items():
+        for i in indices:
+            for j in indices:
+                result.append((i, j))
+
+    return result
+
 
 
 def main_optimized():
