fd83c972b56811829a90e21173d9e08ce7f2e7d0
naumoffIE <asfer364@gmail.com>
2025-12-14 00:00:31 +0400

    добавил в проект и оригинал, чтобы всегда было с чем сравнить + добавил функции для большей читабельности

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,32 +13,44 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASS:
-                if item.quality > self.MIN_QUALITY:
-                    if item.name != self.SULFURAS:
-                        item.quality = item.quality - 1
+                if item.name != self.SULFURAS:
+                    self._decrease_quality(item)
             else:
-                if item.quality < self.MAX_QUALITY:
-                    item.quality = item.quality + 1
-                    if item.name == self.BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < self.MAX_QUALITY:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < self.MAX_QUALITY:
-                                item.quality = item.quality + 1
+                if item.name == self.AGED_BRIE:
+                    self._update_brie_item(item)
+                if item.name == self.BACKSTAGE_PASS:
+                    self._update_backstage_item(item)
             if item.name != self.SULFURAS:
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < self.MIN_QUALITY:
+            if item.sell_in < 0:
                 if item.name != self.AGED_BRIE:
                     if item.name != self.BACKSTAGE_PASS:
-                        if item.quality > self.MIN_QUALITY:
-                            if item.name != self.SULFURAS:
-                                item.quality = item.quality - 1
+                        if item.name != self.SULFURAS:
+                            self._decrease_quality(item)
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
-                    if item.quality < self.MAX_QUALITY:
-                        item.quality = item.quality + 1
+                    self._increase_quality(item)
+
+    def _decrease_quality(self, item):
+        if item.quality > self.MIN_QUALITY:
+            item.quality = item.quality - 1
+
+    def _increase_quality(self, item):
+        if item.quality < self.MAX_QUALITY:
+            item.quality = item.quality + 1
+
+    def _update_brie_item(self, item):
+        self._increase_quality(item)
+
+    def _update_backstage_item(self, item):
+        self._increase_quality(item)
+        if item.sell_in < 11:
+            self._increase_quality(item)
+        if item.sell_in < 6:
+            self._increase_quality(item)
+
+
 
 
 class Item:
